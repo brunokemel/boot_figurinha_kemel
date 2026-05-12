8017fe2065de141ba6a16ee6e8581ad9b8acdfc3
Bruno Kemel <br.kemel@gmail.com>
2026-05-12 08:13:57 -0300

    versão 1.1

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import makeWASocket, {
     downloadMediaMessage,
     DisconnectReason  
 } from "@whiskeysockets/baileys";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 
 // const logger = bool;
@@ -13,13 +16,10 @@ import sharp from "sharp";
 import P from "pino";
 
 async function startBot() {
-    const { state, saveCreds } = await useMultiFileAuthState("auth");
+    // const { state, saveCreds } = await useMultiFileAuthState("auth");
+    const { state, saveCreds } = await useMultiFileAuthState(process.env.AUTH_FOLDER || "auth");
 
-    const sock = makeWASocket({
-        auth: state,
-        logger: P({ level: "silent"}),
-        printQRInTerminal: true
-    });
+    const sock = makeWASocket({auth: state,logger: P({level: process.env.LOG_LEVEL || "silent"}),printQRInTerminal: true});
 
     sock.ev.on("connection.update", (update) => {
         const { connection, qr, lastDisconnect} = update;
@@ -57,7 +57,7 @@ async function startBot() {
         }
 
         
-        const buffer = await downloadMediaMessage(msg, "buffer", {}, { logger: P({ level: "silent" }), reuploadRequest: sock.updateMediaMessage });
+        const buffer = await downloadMediaMessage(msg, "buffer", {}, { logger: P({ level: process.env.LOG_LEVEL || "silent" }), reuploadRequest: sock.updateMediaMessage });
 
         const sticker = await sharp(buffer as Buffer).resize(512, 512, {fit: "contain", background: { r: 0, g: 0, b: 0, alpha: 0}}).webp().toBuffer();
         
